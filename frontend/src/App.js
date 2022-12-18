39768df450902be80018a9d36312dc2b5cd9bca5
ivan954 <ivan3.eng@gmail.com>
2022-12-18 10:00:44 +0200

    update  router changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,78 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen'
-import ProfileScreen from './screens/ProfileScreen'
-import ShippingScreen from './screens/ShippingScreen'
-import PaymentScreen from './screens/PaymentScreen'
-import PlaceOrderScreen from './screens/PlaceOrderScreen'
-import OrderScreen from './screens/OrderScreen'
-import UserListScreen from './screens/UserListScreen'
-import UserEditScreen from './screens/UserEditScreen'
-import ProductListScreen from './screens/ProductListScreen'
-import ProductEditScreen from './screens/ProductEditScreen'
-import OrderListScreen from './screens/OrderListScreen'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
+import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <Route path='/order/:id' component={OrderScreen} />
-          <Route path='/shipping' component={ShippingScreen} />
-          <Route path='/payment' component={PaymentScreen} />
-          <Route path='/placeorder' component={PlaceOrderScreen} />
-          <Route path='/login' component={LoginScreen} />
-          <Route path='/register' component={RegisterScreen} />
-          <Route path='/profile' component={ProfileScreen} />
-          <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/admin/userlist' component={UserListScreen} />
-          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-          <Route
-            path='/admin/productlist'
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path='/admin/productlist/:pageNumber'
-            component={ProductListScreen}
-            exact
-          />
-          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
-          <Route path='/admin/orderlist' component={OrderListScreen} />
-          <Route path='/search/:keyword' component={HomeScreen} exact />
-          <Route path='/page/:pageNumber' component={HomeScreen} exact />
-          <Route
-            path='/search/:keyword/page/:pageNumber'
-            component={HomeScreen}
-            exact
-          />
-          <Route path='/' component={HomeScreen} exact />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  )
-}
+    <>
+      <Router>
+        <Header />
+        <main className="py-3">
+          <div>
+            <Routes>
+              <Route path="/" element={<HomeScreen />}></Route>
+              <Route path="/order/:id" element={<OrderScreen />}></Route>
+              <Route path="/shipping" element={<ShippingScreen />}></Route>
+              <Route path="/payment" element={<PaymentScreen />}></Route>
+              <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
+              <Route path="/login" element={<LoginScreen />}></Route>
+              <Route path="/register" element={<RegisterScreen />}></Route>
+              <Route path="/profile" element={<ProfileScreen />}></Route>
+              <Route path="/product/:id" element={<ProductScreen />}></Route>
+              <Route path="/cart/:id?" element={<CartScreen />}></Route>
+              <Route
+                path="/admin/userlist"
+                element={<UserListScreen />}
+              ></Route>
+              <Route
+                path="/admin/user/:id/edit"
+                element={<UserEditScreen />}
+              ></Route>
+              <Route
+                path="/admin/productlist"
+                element={<ProductListScreen />}
+              ></Route>
+              <Route
+                path="/admin/productlist/:pageNumber"
+                element={<ProductListScreen />}
+              ></Route>
+              <Route
+                path="/admin/product/:id/edit"
+                element={<ProductEditScreen />}
+              ></Route>
+              <Route
+                path="/admin/orderlist"
+                element={<OrderListScreen />}
+              ></Route>
+              <Route path="/search/:keyword" element={<HomeScreen />}></Route>
+              <Route path="/page/:pageNumber" element={<HomeScreen />}></Route>
+              <Route
+                path="/search/:keyword/page/:pageNumber"
+                element={<HomeScreen />}
+              ></Route>
+            </Routes>
+          </div>
+        </main>
+        <Footer />
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,62 +1,64 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import Paginate from '../components/Paginate'
-import ProductCarousel from '../components/ProductCarousel'
-import Meta from '../components/Meta'
-import { listProducts } from '../actions/productActions'
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+import Meta from "../components/Meta";
+import { listProducts } from "../actions/productActions";
 
-const HomeScreen = ({ match }) => {
-	const keyword = match.params.keyword
+const HomeScreen = () => {
+  const params = useParams();
 
-	const pageNumber = match.params.pageNumber || 1
+  const keyword = params.keyword;
 
-	const dispatch = useDispatch()
+  const pageNumber = params.pageNumber || 1;
 
-	const productList = useSelector((state) => state.productList)
-	const { loading, error, products, page, pages } = productList
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(listProducts(keyword, pageNumber))
-	}, [dispatch, keyword, pageNumber])
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
 
-	return (
-		<>
-			<Meta />
-			{!keyword ? (
-				<ProductCarousel />
-			) : (
-				<Link to='/' className='btn btn-light'>
-					Go Back
-				</Link>
-			)}
-			<h1>Latest Products</h1>
-			{loading ? (
-				<Loader />
-			) : error ? (
-				<Message variant='danger'>{error}</Message>
-			) : (
-				<>
-					<Row>
-						{products.map((product) => (
-							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-								<Product product={product} />
-							</Col>
-						))}
-					</Row>
-					<Paginate
-						pages={pages}
-						page={page}
-						keyword={keyword ? keyword : ''}
-					/>
-				</>
-			)}
-		</>
-	)
-}
+  useEffect(() => {
+    dispatch(listProducts(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
 
-export default HomeScreen
+  return (
+    <>
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to="/" className="btn btn-light">
+          Go Back
+        </Link>
+      )}
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
